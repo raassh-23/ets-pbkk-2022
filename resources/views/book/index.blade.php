8e742088b94dab5de44ba6aab1bc1ada3ab44514
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-04-04 00:16:51 +0700

    add not found message

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -6,40 +6,45 @@
 
         <div class="d-flex">
             <div class="col-sm-10">
-                <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-3 row-cols-lg-5 g-3">
-                    @foreach ($books as $book)
-                        <div class="col position-relative">
-                            <div class="card h-100">
-                                <img class="card-image-top" src="{{ $book->cover_image }}" alt="image" style="width:100%">
-                                <div class="card-body d-flex flex-column justify-content-between">
-                                    <div>
-                                        <a class="card-title fw-bold fs-5 stretched-link text-black text-decoration-none m-0"
-                                            href="{{ route('books.show', ['book' => $book->id]) }}">{{ Str::limit($book->title, 30, '...') }}</a>
-                                        <p class="m-0">
-                                            @foreach ($book->writers as $writer)
-                                                {{ $loop->last ? $writer->name : $writer->name . ',' }}
-                                            @endforeach
-                                        </p>
-                                    </div>
-                                    <div class="d-flex flex-row">
-                                        <span style="color: Orange;">
-                                            <i class="fas fa-star"></i>
-                                        </span>
-                                        <p class="ms-1 mb-0">{{ $book->rating ? round($book->rating, 2) : '-' }}</p>
+                @if (count($books) > 0)
+                    <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-3 row-cols-lg-5 g-3">
+                        @foreach ($books as $book)
+                            <div class="col position-relative">
+                                <div class="card h-100">
+                                    <img class="card-image-top" src="{{ $book->cover_image }}" alt="image"
+                                        style="width:100%">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                        <div>
+                                            <a class="card-title fw-bold fs-5 stretched-link text-black text-decoration-none m-0"
+                                                href="{{ route('books.show', ['book' => $book->id]) }}">{{ Str::limit($book->title, 30, '...') }}</a>
+                                            <p class="m-0">
+                                                @foreach ($book->writers as $writer)
+                                                    {{ $loop->last ? $writer->name : $writer->name . ',' }}
+                                                @endforeach
+                                            </p>
+                                        </div>
+                                        <div class="d-flex flex-row">
+                                            <span style="color: Orange;">
+                                                <i class="fas fa-star"></i>
+                                            </span>
+                                            <p class="ms-1 mb-0">{{ $book->rating ? round($book->rating, 2) : '-' }}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
-                </div>
-
+                        @endforeach
+                    </div>
+                @else
+                    <h3 class="text-center">No books found.</h3>
+                @endif
             </div>
             <div class="col-sm-2 ms-3">
                 <div class="row-md-2 mb-3">
                     <h2>Search</h2>
                     <form method="GET" action="{{ route('books.index') }}">
                         <div class="d-flex flex-row">
-                            <input type="text" class="form-control" placeholder="Search books..." name="search">
+                            <input type="text" class="form-control" placeholder="Search books..." name="search" value="{{ request()->search }}">
                             <button type="submit" class="btn btn-primary ms-1"><i class="fa fa-search"></i></button>
                         </div>
                     </form>
--- resources/views/writer/index.blade.php
+++ resources/views/writer/index.blade.php
@@ -4,24 +4,33 @@
     <div class="container">
         <h1>Writers</h1>
         <div class="d-flex">
-            <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
-                @foreach ($writers as $writer)
-                    <div class="col position-relative">
-                        <div class="card h-100">
-                            <img class="card-image-top" src="{{ $writer->image_url }}" alt="{{ $writer->name }}" style="width:100%">
-                            <div class="card-body d-flex flex-column justify-content-between">
-                                <a class="card-title fw-bold fs-5 stretched-link text-black text-decoration-none m-0" href="{{ route('writers.show', ['writer' => $writer->id]) }}">{{  Str::limit($writer->name, 30, '...') }}</a>
-                                <p class="m-0">{{ count($writer->books)}} books</p>
+            <div class="col-md-10">
+                @if (count($writers) > 0)
+                    <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+                        @foreach ($writers as $writer)
+                            <div class="col position-relative">
+                                <div class="card h-100">
+                                    <img class="card-image-top" src="{{ $writer->image_url }}" alt="{{ $writer->name }}"
+                                        style="width:100%">
+                                    <div class="card-body d-flex flex-column justify-content-between">
+                                        <a class="card-title fw-bold fs-5 stretched-link text-black text-decoration-none m-0"
+                                            href="{{ route('writers.show', ['writer' => $writer->id]) }}">{{ Str::limit($writer->name, 30, '...') }}</a>
+                                        <p class="m-0">{{ count($writer->books) }} books</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
-                @endforeach
+                @else
+                    <h3 class="text-center">No writers found.</h3>
+                @endif
             </div>
             <div class="col-md-2 ms-3">
                 <h2>Search</h2>
                 <form method="GET" action="{{ route('writers.index') }}">
                     <div class="d-flex flex-row">
-                        <input type="text" class="form-control" placeholder="Search writers..." name="search">
+                        <input type="text" class="form-control" placeholder="Search writers..." name="search"
+                            value="{{ request()->search }}">
                         <button type="submit" class="btn btn-primary ms-1"><i class="fa fa-search"></i></button>
                     </div>
                 </form>
