d04ccad5feaccb62855ad4d2b9d4bb7d4f8ca3b9
Daanii Kusnanta <danynabilgk@gmail.com>
2022-04-04 00:06:51 +0700

    change publisher detail layout

--- resources/views/publisher/detail.blade.php
+++ resources/views/publisher/detail.blade.php
@@ -2,25 +2,39 @@
 
 @section('content')
     <div class="container">
-        <h1>Publisher Detail</h1>
-        
-        <h2>{{ $publisher->name }}</h2>
-        <p>Address: {{ $publisher->address }}</p>
-        <p>Phone: {{ $publisher->phone }}</p>
-        <p>Email: {{ $publisher->email }}</p>
+        <div class="d-flex flex-row">
+            <img src="{{ $publisher->image_url }}" alt="{{ $publisher->name }}'s cover image" width="200px" height="200px">
+            <div class="d-flex flex-column ms-3 mt-1">
+                <h6 class="text-primary mb-0">PUBLISHER</h6>
+                <h1 class="fw-bolder">{{ $publisher->name }}</h1>
+                <p class="mb-1"><b>Address</b> {{ $publisher->address }}</p>
+                <p class="mb-1"><b>Email</b> {{ $publisher->email }}</p>
+                <p><b>Phone</b> {{ $publisher->phone }}</p>
+            </div>
+        </div>
 
-        <h2>Books</h2>
-        <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+        <h2 class="mt-5">Books ({{ count($publisher->books) ?: 0 }})</h2>
+        <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-3 row-cols-lg-5 g-3">
             @foreach ($publisher->books as $book)
-                <div class="col">
+                <div class="col position-relative">
                     <div class="card h-100">
-                        <img class="card-img-top" src="{{ $book->cover_image }}" alt="image" style="width:100%">
+                        <img class="card-image-top" src="{{ $book->cover_image }}" alt="image" style="width:100%">
                         <div class="card-body d-flex flex-column justify-content-between">
                             <div>
-                                <h4 class="card-title">{{ $book->title }}</h4>
-                                <p class="card-text">{{ Str::limit($book->synopsis, 200, '...') }}</p>
+                                <a class="card-title fw-bold fs-5 stretched-link text-black text-decoration-none m-0"
+                                    href="{{ route('books.show', ['book' => $book->id]) }}">{{ Str::limit($book->title, 30, '...') }}</a>
+                                <p class="m-0">
+                                    @foreach ($book->writers as $writer)
+                                        {{ $loop->last ? $writer->name : $writer->name . ',' }}
+                                    @endforeach
+                                </p>
                             </div>
-                            <a href="{{ route('books.show', ['book' => $book->id]) }}" class="btn btn-primary mt-2">Details</a>
+                            <div class="d-flex flex-row">
+                                <span style="color: Orange;">
+                                    <i class="fas fa-star"></i>
+                                </span>
+                                <p class="ms-1 mb-0">{{ $book->rating ? round($book->rating, 2) : '-' }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
